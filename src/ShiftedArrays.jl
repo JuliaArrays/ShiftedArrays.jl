d7f0836bb33779e063afec5a2061a1ff2a2c99a4
piever <pietro.vertechi@neuro.fchampalimaud.org>
2018-02-12 16:27:47 +0000

    circshift

--- src/ShiftedArrays.jl
+++ src/ShiftedArrays.jl
@@ -6,10 +6,12 @@ if VERSION<v"0.7.0-DEV"
 end
 
 export ShiftedArray, ShiftedVector, shifts
+export CircShiftedArray, CircShiftedVector
 export lag, lead
 export reduce_vec, mapreduce_vec
 
 include("shiftedarray.jl")
+include("circshiftedarray.jl")
 include("lag.jl")
 include("reduce.jl")
 
