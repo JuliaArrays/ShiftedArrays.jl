1e780249265dcdb71e8adcc28be17e20c8ae11db
Matthieu Gomez <gomez.matthieu@gmail.com>
2020-05-06 15:34:32 -0400

    Update lag.jl

--- src/lag.jl
+++ src/lag.jl
@@ -97,3 +97,76 @@ julia> s = lead(v, (0, 2))
 ```
 """
 lead(v::AbstractArray, n = 1; default = missing) = ShiftedArray(v, map(-, n); default = default)
+
+
+"""
+    lag(v::AbstractVector, times::AbstractVector, period = oneunit(eltype(times)); default = missing) -> Vector
+
+Shifts with respect to a times given in the vector `times`.  The third variable `period` gives the period by which to shift.
+`default` specifies a default value when the shifted time is not in `times`.
+Elements in `times` must be all sorted and distinct.
+
+# Examples
+
+```jldoctest lead
+julia> v = [1, 3, 5];
+julia> times = [1990, 1992, 1993];
+julia> lag(v, times)
+3-element Array{Union{Missing, Int64},1}:
+  missing
+  missing
+ 3
+julia> using Dates
+julia> times = [Date(1990, 1, 1), Date(1990, 1, 3), Date(1990, 1, 4)]
+julia> lag(v, times, Day(1))
+3-element Array{Union{Missing, Int64},1}:
+ missing
+ missing
+3
+"""
+
+struct LaggedVector{T <: AbstractVector, U <: AbstractVector, P, M, R} <: AbstractVector{Union{R, M}} 
+  parent::T
+  times::U
+  period::P
+  default::M
+end
+
+function LaggedVector(parent::AbstractVector, times::AbstractVector, period = oneunit(eltype(times)); default = missing)
+  length(parent) == length(times) || error("Values and times must have the same length")
+  is_sorted_and_unique(times)
+  LaggedVector{typeof(parent), typeof(times), typeof(period), typeof(default), eltype(parent)}(parent, times, period, default)
+end
+
+function is_sorted_and_unique(times::AbstractVector)
+  t, rest = Iterators.peel(times)
+  for r in rest
+    r == t && error("Times must be distinct")
+    r < t && error("Times must be sorted")
+    t = r
+  end
+end
+
+parent(s::LaggedVector) = s.parent
+
+Base.size(s::LaggedVector) = size(parent(s))
+
+@inline function getindex(s::LaggedVector{T, U, P, M}, i::Integer) where{T, U, P, M}
+  t = s.times[i] - s.period
+  incr = (s.times[i] >= t) ? 1 : -1
+  while checkbounds(Bool, s.times, i) && ((incr == 1) ? (@inbounds s.times[i] > t) : (@inbounds s.times[i] < t))
+    i -= incr
+  end
+  if checkbounds(Bool, s.times, i) && (@inbounds s.times[i] == t)
+    return s.parent[i]::eltype(T)
+  else
+    return s.default::M
+  end
+end
+
+function lag(v::AbstractVector, times::AbstractVector, period = oneunit(eltype(times)); default = missing)
+    LaggedVector(v, times, period; default = default)
+end
+function lead(v::AbstractVector, times::AbstractVector, period = oneunit(eltype(times)); default = missing)
+    LaggedVector(v, times, -period; default = default)
+end
