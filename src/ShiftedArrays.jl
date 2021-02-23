4ec21e796a17d191969bb53feccffe57fee8a554
Milan Bouchet-Valat <nalimilan@club.fr>
2021-02-23 16:54:12 +0100

    Remove nonexistent exports of reduce_vec and mapreduce_vec (#42)

--- src/ShiftedArrays.jl
+++ src/ShiftedArrays.jl
@@ -4,7 +4,6 @@ import Base: checkbounds, getindex, setindex!, parent, size
 export ShiftedArray, ShiftedVector, shifts, default
 export CircShiftedArray, CircShiftedVector
 export lag, lead
-export reduce_vec, mapreduce_vec
 
 include("shiftedarray.jl")
 include("circshiftedarray.jl")
