f4cd358594e5eb174936f0f4ef5e29d3bb6fb2ba
piever <pietro.vertechi@protonmail.com>
2021-05-18 10:15:05 +0200

    wording in comment

--- src/circshiftedarray.jl
+++ src/circshiftedarray.jl
@@ -28,15 +28,10 @@ julia> copy(s)
 """
 struct CircShiftedArray{T, N, S<:AbstractArray} <: AbstractArray{T, N}
     parent::S
-    # shift stores the circshift but is modified with mod (see below) to be more efficient
-    # in the getindex method
-    # negative shifts (left shift) are converted to positive (right shifts) with a larger
-    # shift number
+    # the field `shifts` stores the circular shifts modulo the size of the parent array
     shifts::NTuple{N, Int}
     function CircShiftedArray(p::AbstractArray{T, N}, n = Tuple(0 for i in 1:N)) where {T, N}
         @assert all(step(x) == 1 for x in axes(p))
-        # n could be a huge shift number, reduce it by taking the mod
-        # shift is also positive due to this mod
         n = map(mod, _padded_tuple(p, n), size(p))
         new{T, N, typeof(p)}(p, n)
     end
